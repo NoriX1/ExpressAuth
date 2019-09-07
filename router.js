9bcf72014b23393c6bbc537b082cb432ae71d723
NoriX <versatility.95@mail.ru>
2019-09-07 21:04:59 +0300

    routes refactored into separate dir

--- router.js
+++ router.js
@@ -1,16 +1,5 @@
-const Authentication = require('./controllers/authentication');
-const passportService = require('./services/passport');
-const passport = require('passport');
-
-const requireAuth = passport.authenticate('jwt', { session: false });   //Auth for requests with token
-const requireSignin = passport.authenticate('local', { session: false });   //Signing in via login and password
+const authRoutes = require('./routes/authRoutes');
 
 module.exports = function (app) {
-    app.get('/', requireAuth, function (req, res) {
-        res.send({ secret: 'data' });
-    })
-    app.post('/signin', requireSignin, Authentication.signin);
-    app.post('/signup', Authentication.signup);
-    app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-    app.get('/auth/google/callback', passport.authenticate('google', {session: false}), Authentication.signInGoogle);
+    authRoutes(app);
 }
